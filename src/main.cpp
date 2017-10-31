058f170920224bc738c014d23f67de04ed0de799
Connor Golobich <cgolobic@gmail.com>
2017-10-30 18:55:50 -0700

    add stb SHL for image loading, basic openGL triangle rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #ifndef __APPLE__
+    #include <SDL_opengl.h>
 	#include <GL/glew.h>  // Don't need GLEW ON MacOS
 #else                   // Include standard MacOS OpenGL headers
-	#include <OpenGL/glu.h>
-	#include <OpenGL/glext.h>
+	//#include <OpenGL/glu.h>
+	//#include <OpenGL/glext.h>
+    #include <OpenGL/gl3.h>
 #endif
-#include <SDL_opengl.h>
 #include "../libs/imgui/imgui.h"
 #include "../libs/imgui/imgui_impl_sdl_gl3.h"
-#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../libs/stb_image.h"
 
 using namespace std;
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
 int main(int argc, char** argv)
 {
+    int x, y, n;
+    unsigned char *data = stbi_load("./assets/03_HiRes_upload.jpg", &x, &y, &n, 0);
+    cout << x << ", " << y << endl;
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		cout << "SDL failed to initialize: " << SDL_GetError() << endl;
@@ -50,6 +63,21 @@ int main(int argc, char** argv)
 	ImGui_ImplSdlGL3_Init(mainWindow);
 
 	bool running = true;
+    GLuint vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+    GLuint vertexBufferID;
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    GLuint programID = LoadShaders("./assets/vertex_shader.vsh", "./assets/fragment_shader.fsh");
+    
   while (running)
   {
 		SDL_Event event;
@@ -65,17 +93,120 @@ int main(int argc, char** argv)
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		ImGui::Render();
+      glUseProgram(programID);
+      glEnableVertexAttribArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDisableVertexAttribArray(0);
+    ImGui::Render();
     SDL_GL_SwapWindow(mainWindow);
     SDL_Delay(16);
   }
 
 	// teardown
+    stbi_image_free(data);
 	ImGui_ImplSdlGL3_Shutdown();
 	SDL_GL_DeleteContext(glContext);
 	SDL_DestroyWindow(mainWindow);
 	SDL_Quit();
 	return 0;
 }
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
+    
+    // Create the shaders
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    // Read the Vertex Shader code from the file
+    std::string VertexShaderCode;
+    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+    if(VertexShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(VertexShaderStream, Line))
+            VertexShaderCode += "\n" + Line;
+        VertexShaderStream.close();
+    }else{
+        printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+        getchar();
+        return 0;
+    }
+    
+    // Read the Fragment Shader code from the file
+    std::string FragmentShaderCode;
+    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+    if(FragmentShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(FragmentShaderStream, Line))
+            FragmentShaderCode += "\n" + Line;
+        FragmentShaderStream.close();
+    }
+    
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+    
+    
+    // Compile Vertex Shader
+    printf("Compiling shader : %s\n", vertex_file_path);
+    char const * VertexSourcePointer = VertexShaderCode.c_str();
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+    glCompileShader(VertexShaderID);
+    
+    // Check Vertex Shader
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+        printf("%s\n", &VertexShaderErrorMessage[0]);
+    }
+    
+    
+    
+    // Compile Fragment Shader
+    printf("Compiling shader : %s\n", fragment_file_path);
+    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+    glCompileShader(FragmentShaderID);
+    
+    // Check Fragment Shader
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
+        glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+        printf("%s\n", &FragmentShaderErrorMessage[0]);
+    }
+    
+    
+    
+    // Link the program
+    printf("Linking program\n");
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+    
+    // Check the program
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> ProgramErrorMessage(InfoLogLength+1);
+        glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        printf("%s\n", &ProgramErrorMessage[0]);
+    }
+    
+    
+    glDetachShader(ProgramID, VertexShaderID);
+    glDetachShader(ProgramID, FragmentShaderID);
+    
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+    
+    return ProgramID;
+}
+
+
